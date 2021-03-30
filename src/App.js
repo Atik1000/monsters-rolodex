74ab67a2d832350192e071f84f88926363f73bef
Atik1000 <foysalkazi45@gmail.com>
2021-03-31 02:13:36 +0600

    filtering issue

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import CardList from "./components/Card-list/card-list.component";
 
 const App = () => {
   const [monsters, setmonsters] = useState([]);
+  const [value, setValue] = useState('');
+  const handleChange = (e) => setValue(e.target.value);
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then(function (res) {
         setmonsters(res.data);
-        console.log(res, "===success");
       })
       .catch(function (err) {
         console.log(err);
       });
   }, []);
+  const filterMonsters=monsters.filter(monsters=>{
+    monsters.name.toLowerCase().include(value.toLowerCase)
+  })
 
   return (
     <div className="App">
+     <input type="text" value={value} onChange={handleChange} />
+     {value}
       <CardList monsters={monsters} />
     </div>
   );
