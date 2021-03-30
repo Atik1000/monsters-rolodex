4d6e1b050ee594f474a71d8edcd1468a8393a5a6
Atik1000 <foysalkazi45@gmail.com>
2021-03-31 01:03:09 +0600

    fixed problem

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import CardList from "./components/Card-list/card-list.component";
 
-
 const App = () => {
   const [monsters, setmonsters] = useState([]);
-useEffect(()=>{
- fetch('https://jsonplaceholder.typicode.com/users')
- .then(Response => Response.json())
- .then(users => setmonsters({ monsters: users }))
- console.log(Response);
-})
- 
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(function (res) {
+        setmonsters(res.data);
+        console.log(res, "===success");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <CardList monsters={monsters} />
